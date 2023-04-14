dccea8ee6668cb0fd3be04718f53a2395346480f
LorisMarzocchi <ml77182a@gmail.com>
2023-04-14 17:45:40 +0200

    addstyle

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,20 @@
 
 let num = parseInt(prompt('Scrivi un numero'));
-const eleGrid = document.querySelector('.container_num');
+const cont_num = document.querySelector('.container_num');
 for (let i = 1; i <= num; i++) {
-    console.log(i);
-    eleGrid.innerHTML = eleGrid.innerHTML + `<div class="number">${i}</div>`;
+ 
+    if (i % 3 === 0 && i % 5 === 0){
+     cont_num.innerHTML = cont_num.innerHTML + `<div class="number fizzBuzz">FizzBuzz</div>`;
+    }
+   else if(i % 3 === 0){
+    cont_num.innerHTML = cont_num.innerHTML + `<div class="number fizz">Fizz</div>`;
+   }
+   else if (i % 5 === 0) {
+    cont_num.innerHTML = cont_num.innerHTML + `<div class="number buzz">Buzz</div>`;
+   }
+
+   else{
+    cont_num.innerHTML = cont_num.innerHTML + `<div class="number">${i}</div>`;
+   }
+
 }
